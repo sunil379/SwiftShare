316e82e05110e12a12b83a7d92b34c14ef17b7cd
sunil379 <dewangansunil2003@gmail.com>
2024-03-12 00:03:15 +0530

    modified entire code.

--- lib/Owner/own_forget_pass.dart
+++ lib/Owner/own_forget_pass.dart
@@ -1,23 +1,37 @@
-// forget password
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class ForgetPasswordScreen extends StatelessWidget {
+class ForgetPasswordScreen extends StatefulWidget {
   final FirebaseAuth auth;
   final TextEditingController emailController;
   const ForgetPasswordScreen(
       {super.key, required this.emailController, required this.auth});
 
+  @override
+  _ForgetPasswordScreenState createState() => _ForgetPasswordScreenState();
+}
+
+class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
+  final TextEditingController newPasswordController = TextEditingController();
+  bool _showPasswordResetForm = false;
+  bool _isLoading = false;
+
   Future<void> _resetPassword(BuildContext context) async {
+    setState(() {
+      _isLoading = true;
+    });
     try {
-      String email = emailController.text.trim();
+      String email = widget.emailController.text.trim();
       if (email.isNotEmpty) {
-        await auth.sendPasswordResetEmail(email: email);
+        await widget.auth.sendPasswordResetEmail(email: email);
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Password reset email sent to $email')),
         );
+        setState(() {
+          _showPasswordResetForm = true;
+        });
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Please enter your email address')),
@@ -27,6 +41,35 @@ class ForgetPasswordScreen extends StatelessWidget {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Failed to send password reset email : $error')),
       );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _changePassword(BuildContext context) async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      User? user = widget.auth.currentUser;
+      if (user != null) {
+        String newPassword = newPasswordController.text.trim();
+        await user.updatePassword(newPassword);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Password changed successfully')),
+        );
+        // You might want to navigate the user back to login screen or any other screen after password change
+      }
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to change password: $error')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
@@ -38,56 +81,101 @@ class ForgetPasswordScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Forget Your Password?',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const Text(
-              'Please enter your registered email address to reset your password',
-              style: TextStyle(
-                fontSize: 12,
-                color: Colors.grey,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(labelText: 'Email Address'),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _resetPassword(context);
-              },
-              child: const Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 24,
-                ),
-                child: Text(
-                  'Reset Password',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+        child: _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : _showPasswordResetForm
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Enter New Password',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      TextField(
+                        controller: newPasswordController,
+                        decoration:
+                            const InputDecoration(labelText: 'New Password'),
+                        obscureText: true,
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          _changePassword(context);
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 12,
+                            horizontal: 24,
+                          ),
+                          child: Text(
+                            'Change Password',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Forget Your Password?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      const Text(
+                        'Please enter your registered email address to reset your password',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      TextField(
+                        controller: widget.emailController,
+                        decoration:
+                            const InputDecoration(labelText: 'Email Address'),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          _resetPassword(context);
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 12,
+                            horizontal: 24,
+                          ),
+                          child: Text(
+                            'Reset Password',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                ),
-              ),
-            )
-          ],
-        ),
       ),
     );
   }
