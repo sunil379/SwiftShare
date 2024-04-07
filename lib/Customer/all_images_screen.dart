fd4d615467f8c7e8565df1c36d8e2d8e86b16b2e
sunil379 <dewangansunil2003@gmail.com>
2024-04-07 17:18:32 +0530

    7th April.

--- lib/Customer/all_images_screen.dart
+++ lib/Customer/all_images_screen.dart
@@ -18,7 +18,31 @@ class AllImagesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('All Images'),
+        centerTitle: true,
+        title: const Text('All Vehicles'),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            width: 45,
+            height: 45,
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(15),
+                ),
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                )),
+            child: const Icon(
+              Icons.keyboard_arrow_left,
+              color: Colors.black,
+            ),
+          ),
+        ),
       ),
       body: ListView.builder(
         itemCount: imageUrls.length,
--- lib/Customer/customervehicleinfo.dart
+++ lib/Customer/customervehicleinfo.dart
@@ -85,8 +85,39 @@ class _CarInfoPageState extends State<CarInfoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[90],
       appBar: AppBar(
-        title: const Text('Vehicle Info'),
+        // backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: const Text(
+          'Vehicle Info',
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            width: 45,
+            height: 45,
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(15),
+                ),
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                )),
+            child: const Icon(
+              Icons.keyboard_arrow_left,
+              color: Colors.black,
+            ),
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
