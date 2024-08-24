03238e8201c4e100f43ceca5418a89c7d4dd8d5b
sunil379 <dewangansunil2003@gmail.com>
2024-08-25 00:56:05 +0530

    changed directory.

--- lib/Customer/navigation_drawer.dart
+++ lib/Customer/navigation_drawer.dart
@@ -1,12 +1,11 @@
 // ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:swiftshare_one/Customer/customer_account.dart';
-import 'package:swiftshare_one/Customer/customer_login.dart';
-import 'package:swiftshare_one/Customer/customer_rateapp.dart';
-import 'package:swiftshare_one/Customer/customer_refer_earn.dart';
+import 'package:swiftshare_one/screens/rateapp.dart';
+import 'package:swiftshare_one/screens/refer_earn.dart';
+import 'package:swiftshare_one/pages/app_start.dart';
+import '../screens/about_us.dart';
 
 class NavigationDrawers extends StatefulWidget {
   final String initialUserName;
@@ -38,7 +37,7 @@ class _NavigationDrawerState extends State<NavigationDrawers> {
       await FirebaseAuth.instance.signOut();
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
-          builder: (context) => CustomerLoginScreen(),
+          builder: (context) => const EntryScreen(),
         ),
       );
     } catch (e) {
@@ -213,106 +212,6 @@ class _NavigationDrawerState extends State<NavigationDrawers> {
             onTap: () {
               _signOut();
             },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AboutUsScreen extends StatelessWidget {
-  const AboutUsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'About Us',
-          style: TextStyle(
-            fontSize: 28,
-          ),
-        ),
-        centerTitle: true,
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Container(
-            width: 45,
-            height: 45,
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(15),
-              ),
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
-              ),
-            ),
-            child: const Icon(
-              Icons.keyboard_arrow_left,
-              color: Colors.black,
-            ),
-          ),
-        ),
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            width: 400,
-            child: Row(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/rectangle-7.png'),
-                  width: 150,
-                  height: 120,
-                  fit: BoxFit.cover,
-                ),
-                Text(
-                  'SWIFTSHARE',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue,
-                    letterSpacing: 2,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            width: 500,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 8,
-              vertical: 8,
-            ),
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              border: Border.all(
-                color: Colors.black.withOpacity(0.4),
-                width: 2,
-              ),
-            ),
-            child: const Expanded(
-              child: SingleChildScrollView(
-                padding: EdgeInsets.all(20),
-                scrollDirection: Axis.vertical,
-                child: Text(
-                  "SwiftShare is a platform designed to simplify and streamline the process of renting various types of vehicles. With a diverse fleet of bikes, scooters, cars, and e-vehicles.\n\n SwiftShare ensures a seamless experience from browsing required vehicles to quick reservation. Offering flexible rental options ranging from hourly, weekly to monthly durations.\n\n SwiftShare caters to both locals and tourists, offering a convenient and eco-friendly alternative to traditional transportation methods.",
-                  softWrap: true,
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize: 18,
-                    // fontWeight: FontWeight.bold,
-                    letterSpacing: 1,
-                  ),
-                ),
-              ),
-            ),
           ),
         ],
       ),
