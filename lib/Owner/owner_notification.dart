3346813189eb57f8a3e05d87bcb2a9b63668450e
sunil379 <dewangansunil2003@gmail.com>
2024-08-25 23:08:16 +0530

    notification

---
+++ lib/Owner/owner_notification.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class NotificationsPage extends StatelessWidget {
+  const NotificationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Notifications',
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            width: 45,
+            height: 45,
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(15),
+                ),
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                )),
+            child: const Icon(
+              Icons.keyboard_arrow_left,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'No Notifications here !!!',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
